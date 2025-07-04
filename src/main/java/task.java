fefd43c7b150b166d552938782b2b3174bd55a39
Abdurakhman <sayfya040@gmail.com>
2025-07-04 09:51:51 +0600

    FootBall

--- src/main/java/task.java
+++ src/main/java/task.java
@@ -2,16 +2,8 @@ public class task {
 
 
     public static void main(String[] args) {
-    }
 
 
-    public String notString(String str) {
-
-        if(str.length() >= 3 || str.substring(0).equals("not")){
-            return str;
-
-        }
- return "not" + str;
     }
 
 }
